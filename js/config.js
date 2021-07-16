308b685f5adfaac6886022537eb24774950ba577
Mohsan Jaffery <mohsan.jaffery@blackspring.co.uk>
2021-07-16 11:27:46 +0100

    integrate apigw again

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_7SmzuJ1cu', 
-        userPoolClientId: '56ohffpstko9odhl25ejjf6pj4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1' // e.g. us-east-2
+        userPoolClientId: '56ohffpstko9odhl25ejjf6pj4',
+        region: 'us-east-1'
     },
     api: {
-        invokeUrl: 'https://mufl9ocpnj.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://mufl9ocpnj.execute-api.us-east-1.amazonaws.com/prod'
     }
 };
